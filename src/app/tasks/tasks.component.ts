0c8f5172757932899600861f670e96b9228e4261
Mario Ignatov <mario.ignatov@qualifast.bg>
2018-08-06 13:19:11 +0300

    Implementing the listing of latest tasks in the Tasks page

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  task = {
+    time: new Date(),
+    event: 'Lost luggage',
+    voters: {
+      current: 1,
+      total: 7
+    },
+    country: 'GB',
+    type: 'transport'
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
