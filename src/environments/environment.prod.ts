38c69272734c67d0846125662110fab96185cbae
Mario Ignatov <mario.ignatov@qualifast.bg>
2018-08-15 15:36:52 +0300

    Adding Google Maps API key to the environment configurations; Updating the package.json with the Google Maps and Media player libraries

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyAOD_Y71qt63qxFXPLeKtQdw8nQnczzqv8'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyAOD_Y71qt63qxFXPLeKtQdw8nQnczzqv8'
 };
 
 /*
