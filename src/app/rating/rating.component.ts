3c32f900ea619a1f956307614338f482f3d0df72
Mario Ignatov <mario.ignatov@qualifast.bg>
2018-08-15 11:22:45 +0300

    Implementing rating component with stars

---
+++ src/app/rating/rating.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss']
+})
+export class RatingComponent implements OnInit {
+
+  stars: string[];
+
+  @Input() rating: number;
+  @Input() max = 5;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    if (this.rating > this.max) {
+      this.rating = this.max;
+    }
+
+    this.stars = Array(this.max).fill('/assets/ic_small_star.svg', 0, this.rating).fill('/assets/ic_small_star_empty.svg', this.rating, this.max);
+  }
+
+}
